d381a0c26c40186fdc042df291109371935a67f5
Lucas Candido <lucascpn05@gmail.com>
2020-06-23 15:12:29 -0300

    Adding of Cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
-
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(routes);
